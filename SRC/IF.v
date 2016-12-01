58da0727fd4ab8a74d8daca31d901c4197d916ca
Death <lkwq007@gmail.com>
2016-12-01 11:53:35 +0800

    Trim IF

--- SRC/IF.v
+++ SRC/IF.v
@@ -19,43 +19,43 @@
 //
 //////////////////////////////////////////////////////////////////////////////////
 module IF(clk,reset,Z,J,JR,PC_IFWrite,JumpAddr,
-           JrAddr,BranchAddr,Instruction_if,PC,NextPC_if);
-    input clk;
-    input reset;
-    input Z;
-    input J;
-    input JR;
-    input PC_IFWrite;
-    input [31:0] JumpAddr;
-    input [31:0] JrAddr;
-    input [31:0] BranchAddr;
-    output [31:0] Instruction_if;
-    output [31:0] PC,NextPC_if;
-    reg PC;
-// MUX for PC
-    reg[31:0] PC_in;
-    always @(*) begin
-        case({JR,J,Z})
-        1:PC_in<=BranchAddr;
-        2:PC_in<=JumpAddr;
-        4:PC_in<=JrAddr;
-        default:PC_in<=NextPC_if;
-    end
+		   JrAddr,BranchAddr,Instruction_if,PC,NextPC_if);
+	input clk;
+	input reset;
+	input Z;
+	input J;
+	input JR;
+	input PC_IFWrite;
+	input [31:0] JumpAddr;
+	input [31:0] JrAddr;
+	input [31:0] BranchAddr;
+	output [31:0] Instruction_if;
+	output [31:0] PC,NextPC_if;
+	reg PC;
 
-	
-//PC REG
-    always @(posedge clk) begin
-        if(PC_IFWrite) begin
-            PC=PC_in;
-        end
-    end
-     
-//Adder for NextPC
-    adder_32bits pcAdd4(.a(pc),.b({29'b0,3'b100}),.ci(1'b0),.s(NextPC_if),.co());
+	// MUX for PC
+	reg[31:0] PC_in;
+	always @(*) begin
+		case({JR,J,Z})
+		1:PC_in<=BranchAddr;
+		2:PC_in<=JumpAddr;
+		4:PC_in<=JrAddr;
+		default:PC_in<=NextPC_if;
+	end
+
+	//PC REG
+	always @(posedge clk) begin
+		if(PC_IFWrite) begin
+			PC=PC_in;
+		end
+	end
+	 
+	//Adder for NextPC
+	adder_32bits pcAdd4(.a(pc),.b({29'b0,3'b100}),.ci(1'b0),.s(NextPC_if),.co());
   
-//ROM
-InstructionROM  InstructionROM(
-	.addr(PC[7:2]),
-	.dout(Instruction_if));
+	//ROM
+	InstructionROM InstructionROM(
+		.addr(PC[7:2]),
+		.dout(Instruction_if));
 	
 endmodule
